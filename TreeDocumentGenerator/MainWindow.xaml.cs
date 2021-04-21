55bfa176d2c8909c1aebcb1c26a1eef49ab9dc57
Mahdi Arabpour <Mahdi.A2000@outlook.com>
2021-04-21 20:50:01 +0430

    added plaque image functionallity

--- TreeDocumentGenerator/MainWindow.xaml.cs
+++ TreeDocumentGenerator/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace TreeDocumentGenerator
 {
@@ -46,9 +49,71 @@ namespace TreeDocumentGenerator
 
         private void PlaqueImagePath_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
-            if (File.Exists(PlaqueImagePath.Text))
+            var plaqueImagePath = PlaqueImagePath.Text.Trim();
+            if (File.Exists(plaqueImagePath))
             {
+                AddPlaqueImage(plaqueImagePath);
             }
+        }
+
+        private void AddPlaqueImage(string plaqueImagePath)
+        {
+            var cropedPlaqueImage = CropPlaqueImage(plaqueImagePath);
+
+            Bitmap templateImage = resizeImage(Image.FromFile("Images/TreeDocument.png") as Bitmap, new System.Drawing.Size(1080, 1080));
+
+
+            Bitmap img = new
+                Bitmap(templateImage.Width, templateImage.Height);
+            using (Graphics gr = Graphics.FromImage(img))
+            {
+                gr.DrawImage(cropedPlaqueImage, new System.Drawing.Point((templateImage.Width / 2), 0));
+                gr.DrawImage(templateImage, new System.Drawing.Point(0, 0));
+            }
+            OutputImage.Source = BitmapToImageSource(img);
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private Bitmap resizeImage(Bitmap imgToResize, System.Drawing.Size size)
+        {
+            return (new Bitmap(imgToResize, size));
+        }
+
+        private Bitmap CropPlaqueImage(string plaqueImagePath)
+        {
+            Bitmap src = Image.FromFile(plaqueImagePath) as Bitmap;
+
+            Rectangle cropRect = new Rectangle((src.Height / 8), 0, (src.Height / 2), src.Height);
+
+            Bitmap target = new Bitmap(cropRect.Width, cropRect.Height);
+
+            using (Graphics g = Graphics.FromImage(target))
+            {
+                g.DrawImage(src, new Rectangle(0, 0, target.Width, target.Height),
+                                 cropRect,
+                                 GraphicsUnit.Pixel);
+            }
+            return resizeImage(target, new System.Drawing.Size(540, 1080));
+        }
+
+        private void UpdateImage()
+        {
+            OutputImage.Source = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + @"\output.png", UriKind.Absolute));
         }
     }
 }
